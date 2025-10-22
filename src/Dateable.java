a1b93573fd259768a1decd8cbf98cc7f7fb65666
aiden <aideny2028@student.cis.edu.hk>
2025-10-22 15:52:34 +0800

    Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int Age = 7 + (age/2);
+                System.out.println(age+ "-yearolds should date somebody who is at least " + Age + " Years old.");
+        scanner.close();
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+Scanner scanner = new Scanner(System.in);
+int age = scanner.nextInt();
+int pages = 100 - age;
+System.out.println(age + "-yearolds should read at least " + pages + " pages before giving up");
+scanner.close();
     }
 }
